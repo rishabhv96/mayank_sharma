30c4e8910c0d9946bdaca2ba46367aa7d56fe425
InHall Technologies <inhall.updatebackup@gmail.com>
2022-04-24 02:16:57 +0530

    First commit

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/greenhound/Desktop/InHall/ML/mayank/env/lib"
+XML2_LIBS="-lxml2 -L/home/greenhound/Desktop/InHall/ML/mayank/env/lib -lz -L/home/greenhound/Desktop/InHall/ML/mayank/env/lib -llzma -lpthread -L/home/greenhound/Desktop/InHall/ML/mayank/env/lib -L/home/greenhound/Desktop/InHall/ML/mayank/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/greenhound/Desktop/InHall/ML/mayank/env/include/libxml2 -I/home/greenhound/Desktop/InHall/ML/mayank/env/include"
+MODULE_VERSION="xml2-2.9.12"
+
